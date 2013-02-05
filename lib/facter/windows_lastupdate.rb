cdb51ed4808653e91fe010133d8251810ff9866a
Justin Stoller <justin@puppetlabs.com>
2013-02-05 11:53:48 -0800

    you're an escape sequence

--- lib/facter/windows_lastupdate.rb
+++ lib/facter/windows_lastupdate.rb
@@ -2,8 +2,8 @@ Facter.add( :last_update ) do
   confine :kernel => "windows"
   setcode do
     output = Facter::Util::Resolution.exec(
-      "reg query 'HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion" +
-      "\WindowsUpdate\Auto Update\Results\Install' /v LastSuccessTime"
+      'reg query \'HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion' +
+      '\WindowsUpdate\Auto Update\Results\Install\' /v LastSuccessTime'
     ).strip
     date, time = output.split(/\s+/)[2,3]
     year, month, day = date.split('-')
