fa1944c887c0abed9328f049b34fd4ab930b648d
Justin Stoller <justin@puppetlabs.com>
2013-02-05 11:40:41 -0800

    initial commit

---
+++ Modulefile
@@ -0,0 +1,10 @@
+name    'justinstoller/windows_facts'
+version '0.0.1'
+source 'https://github.com/justinstoller/puppet-windows_facts.git'
+author 'justinstoller'
+license 'Apache License, Version 2.0'
+summary 'Additional Facts for Windows Hosts'
+description 'Facts Puppetlabs uses in addition to adenning/winfacts, currently only includes time since last system update'
+project_page 'https://github.com/justinstoller/puppet-windows_facts.git'
+
+# You should probably also be using adenning's winfacts module
---
+++ lib/facter/windows_lastupdate.rb
@@ -0,0 +1,15 @@
+Facter.add( :last_update ) do
+  confine :kernel => "windows"
+  setcode do
+    output = Facter::Util::Resolution.exec(
+      "reg query 'HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion" +
+      "\WindowsUpdate\Auto Update\Results\Install' /v LastSuccessTime"
+    ).strip
+    date, time = output.split(/\s+/)[2,3]
+    year, month, day = date.split('-')
+    hour, minute, second = time.split(':')
+    local = Time.local(year, month, day, hour, minute, second)
+
+    return local.utc.to_s
+  end
+end
